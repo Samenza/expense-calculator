7a7dde47d6d1e4e7413042195a1e0ca000be37c6
samenza <saeid.amiznadeh0@gmail.com>
2022-06-12 00:00:58 +0430

    feat: set base api url in env variable

--- src/configs/axiosConfigs.js
+++ src/configs/axiosConfigs.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 // baseURL: "https://expense-calculator-production.up.railway.app",
 export const api = axios.create({
-  baseURL: "http://192.168.1.161:4000",
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
